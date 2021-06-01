2e91b9ec5f06f203349c64570db5ba7f251a5c82
rojay2010 <84771727+rojay2010@users.noreply.github.com>
2021-06-01 15:59:32 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include <math.h>
 
+
+float frandom() {
+  long int q = random();
+  float ret = (float)q/(float)RAND_MAX;
+  return ret;
+}
+
 float mc_pi(int a)
 {
   float x=0,y=0;
@@ -23,11 +30,6 @@ float mc_pi(int a)
   return (4.0*circle)/a;
 }
 
-float frandom() {
-  long int q = random();
-  float ret = (float)q/(float)RAND_MAX;
-  return ret;
-}
 
 int main(void) {
   float pi0;
